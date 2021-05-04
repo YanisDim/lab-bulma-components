5df93ac200e78aa9001cf11234cfda7b211170e4
Yanis Dimitropoulos <yanis.dim@gmail.com>
2021-05-04 05:11:22 +0200

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import Navbar from './navbar/Navbar'
 import FormField from './formfield/FormField'
 import CoolButton from './coolbutton/CoolButton'
+import Signup from './signup/Signup'
 import 'bulma/css/bulma.css';
 
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/*<Navbar />
       <FormField label="Name" type='text' placeholder='e.g Alex Smith'/>
       <FormField label="Email" type='Email' placeholder='e.g. alexsmith@gmail.com'/>
+      <FormField label="Password" type="password" placeholder="Your Password" />
       <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
-      <CoolButton isSmall isSuccess>Button 2</CoolButton>
+      <CoolButton isSmall isSuccess>Submit</CoolButton>*/}
+      <Signup />
     </div>
     
 
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'bulma/css/bulma.css';
-
+import CoolButton from '../coolbutton/CoolButton'
 
 const Navbar= ()=>{
     return(
@@ -50,29 +50,14 @@ const Navbar= ()=>{
           </a>
         </div>
       </div>
+      
     </div>
 
     <div className="navbar-end">
       <div className="navbar-item">
         <div className="field is-grouped">
-          <p className="control">
-            <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-              <span className="icon">
-                <i className="fab fa-twitter"></i>
-              </span>
-              <span>
-                Tweet
-              </span>
-            </a>
-          </p>
-          <p className="control">
-            <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.2/bulma-0.9.2.zip">
-              <span className="icon">
-                <i className="fas fa-download"></i>
-              </span>
-              <span>Download</span>
-            </a>
-          </p>
+          <CoolButton>Log in</CoolButton>
+          <CoolButton>Sign up</CoolButton>
         </div>
       </div>
     </div>
